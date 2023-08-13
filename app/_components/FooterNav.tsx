7fbbbd3fee2f714b9c0d1c1458f308a56b5fbf16
ssikors2 <01169235@pw.edu.pl>
2023-08-13 16:40:12 +0200

    minor fix

--- app/_components/FooterNav.tsx
+++ app/_components/FooterNav.tsx
@@ -1,7 +1,7 @@
 
 export const FooterNav: React.FC = () => {
   return <>
-    <div className="flex flex-row justify-evenly w-full items-center w-full h-16 fixed bg-[#202020] bottom-0 text-white">
+    <div className="flex flex-row justify-evenly items-center w-full h-16 fixed bg-[#202020] bottom-0 text-white">
       <div>Stats</div>
       <div>Workout</div>
       <div>Templates</div>
