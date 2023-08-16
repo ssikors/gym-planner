9c1a3c727cf22689b3a05b0cac50340b105f7250
ssikors2 <01169235@pw.edu.pl>
2023-08-16 21:28:09 +0200

    added login screen component

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,9 @@
+import { LogInScreen } from "./_components/LogInScreen";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
-      <h1 className="font-semibold">Hello World</h1>
-      <p>
-        eligendi. Laudantium, corrupti! Molestiae tempora aut quo culpa
-        consequatur velit facere, nemo architecto dicta, reprehenderit
-        molestias! Corporis itaque quaerat eaque vitae consectetur deserunt
-        magnam sit molestiae temporibus? Quo a voluptates tempore quos quam
-        dolor quaerat aut deserunt delectus animi modi sapiente voluptas
-        repellat asperiores aliquid doloribus consequuntur voluptatibus
-        laudantium incidunt, corporis magnam quasi. Quia voluptates accusamus
-        optio reprehenderit natus debitis, quos, molestias officiis recusandae
-        atque facere pariatur provident consequatur vel aliquam ut. Nisi
-        molestias neque minima sunt quos rerum sit nesciunt, nostrum laboriosam,
-        tempore esse, delectus ut. Ratione quis distinctio asperiores dolores
-        amet, quas in nulla adipisci laborum sint maxime laudantium ut que ab
-        illo labore alias unde neque ipsam natus? Laborum tenetur reprehenderit
-        unde magnam aut ipsam dolorem delectus qui nulla illum! Quaerat
-        consequatur adipisci unde suscipit architecto doloremque, ut quibusdam
-        accusamus, consectetur reprehenderit ipsum sunt sapiente! Assumenda,
-        quibusdam autem. Saepe repellendus unde odit impedit excepturi dicta
-        ratione ducimus laboriosam quia, earum cupiditate laborum temporibus
-        doloremque sint eveniet fugit corrupti doloribus consequuntur, natus,
-        aliquam nulla. Officiis rem tenetur amet itaque a saepe repudiandae
-        debitis ullam minima excepturi modi quidem optio corrupti minus
-        aspernatur, ut molestiae eum ipsum maiores ex consequatur error, earum
-        veritatis! Similique veniam eos suscipit necessitatibus nostrum
-        perferendis corrupti ad porro laboriosam, ipsum quibusdam sunt, officia
-        eligendi, minima ab. Maxime, reprehenderit. Vitae quos explicabo
-        provident numquam dolorum. Itaque incidunt nihil, iure voluptate fugiat
-        id nesciunt quo architecto sed obcaecati nulla eaque ratione, molestiae
-        quos voluptas modi. Molestias voluptate dolorum repellat illo doloremque
-        ducimus quaerat molestiae. Porro t dolore molestiae est libero aut. Ad
-        eius sunt quidem ratione iusto eligendi?
-      </p>
+      <LogInScreen />
     </main>
   );
 }
