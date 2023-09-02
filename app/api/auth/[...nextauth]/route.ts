e4824852c25d5d03765505a2bd88d16e19cd20f7
ssikors2 <01169235@pw.edu.pl>
2023-09-02 21:59:30 +0200

    set up authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,27 @@ import NextAuth from "next-auth/next"
 import bcrypt from "bcrypt"
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const authOptions = {
-  adapter: PrismaAdapter
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password"}
+      },
+      async authorize(credentials) {
+
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt" // using json web tokens
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === "development"
 }
