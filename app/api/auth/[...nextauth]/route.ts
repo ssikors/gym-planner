654cf39c843862ddb7b3e952e1412110157e4e06
ssikors2 <01169235@pw.edu.pl>
2023-09-02 21:34:57 +0200

    Added Prisma

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,8 @@
 import NextAuth from "next-auth/next"
 import bcrypt from "bcrypt"
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+export const authOptions = {
+  adapter: PrismaAdapter
+}
