a1075e26ecceef8ecb23bec9fe96d5fb196fd8de
ssikors2 <01169235@pw.edu.pl>
2023-09-05 22:14:27 +0200

    Implemented logout + created its component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 'use client'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
+import { LoginLogout } from './components/LoginLogout'
 
 export default function Home() {
   const { data: session, status } = useSession()
-  console.log(session)
   return (
     <main>
-      <h1></h1>
-      <Link href="/register">Register</Link>
-      <Link href="/log-in">Log in</Link>
+      <LoginLogout/>
     </main>
   )
 }
