bf5e7cea307cdaf82d7591c5febb379147cddb8f
ssikors2 <01169235@pw.edu.pl>
2023-09-10 17:21:30 +0200

    Styled LoginLogout

--- app/components/LoginLogout.tsx
+++ app/components/LoginLogout.tsx
@@ -14,16 +14,16 @@ export const LoginLogout: React.FC<Props> = () => {
         {status === "loading" ? (
           "Loading..."
         ) : status === "unauthenticated" ? (
-          <div className="flex flex-row gap-8">
-            <div>
+          <div className="flex flex-col items-center gap-8">
+            <div className="hover:cursor-pointer flex justify-center w-36 py-2 bg-teal-900 ring-2 ring-teal-300 shadow-md hover:scale-105 hover:opacity-60">
               <Link href="/signin">Register</Link>
             </div>
-            <div>
+            <div className="hover:cursor-pointer flex justify-center w-36 py-2 bg-teal-900 ring-2 ring-teal-300 shadow-md hover:scale-105 hover:opacity-60">
               <Link href="/log-in">Log in</Link>
             </div>
           </div>
         ) : (
-          <button onClick={() => signOut()}>Log out</button>
+          <button className="hover:cursor-pointer  px-8 py-2 bg-teal-900 ring-2 ring-teal-300 shadow-md hover:scale-105 hover:opacity-60" onClick={() => signOut()}>Log out</button>
         )}
       </div>
     </>
