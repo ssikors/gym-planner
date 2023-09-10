cd79dcd98fb28bdff2d0c9c60e92489e4889fd62
ssikors2 <01169235@pw.edu.pl>
2023-09-10 17:07:26 +0200

    Added SimpleModal

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-'use client'
-import { useSession } from 'next-auth/react'
-import Link from 'next/link'
-import { LoginLogout } from './components/LoginLogout'
+"use client";
+import { useSession } from "next-auth/react";
+import { SimpleModal } from "./components/util/SimpleModal";
+import { LoginLogout } from "./components/LoginLogout";
 
 export default function Home() {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
   return (
     <main>
-      <LoginLogout/>
+      <div className="flex w-full h-screen items-center justify-center bg-gradient-to-b from-teal-300 to-black">
+        <SimpleModal>
+          <LoginLogout />
+        </SimpleModal>
+      </div>
     </main>
-  )
+  );
 }
