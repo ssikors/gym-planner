3a7e2001893ddc649ff7c6c26eb81e17870a3b20
ssikors2 <01169235@pw.edu.pl>
2023-09-11 20:20:18 +0200

    Added more pages

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   const { data: session, status } = useSession();
   return (
     <>
-      <Header />
       <div className="flex w-full h-screen items-center justify-center bg-gradient-to-b from-teal-300 to-black">
         <SimpleModal>
           <LoginLogout />
