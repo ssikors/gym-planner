780d8b9384ab09b47868f47aad578b5324169f18
ssikors2 <01169235@pw.edu.pl>
2023-09-11 16:08:54 +0200

    Logout and page name in header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,15 @@
-export const Header = () => {
+import { LoginLogout } from "./LoginLogout"
+
+type Props = {
+  currentPage?: string
+}
+
+export const Header: React.FC<Props> = ({currentPage}) => {
   return <div className="text-white fixed flex justify-between items-center top-0 left-0 w-full h-24 bg-slate-800 border-y-4 border-y-teal-200">
-    <div>Logged in</div>
-    <div>Settings</div>
+    <div className="ml-4 text-xl font-semibold">{currentPage}</div>
+    <div className="mr-4">
+    <LoginLogout/>
+    </div>
+
   </div>
 }
