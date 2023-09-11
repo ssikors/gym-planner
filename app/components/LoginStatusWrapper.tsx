86f2101ea7e18619b93fe4115c58e68c54a21dd3
ssikors2 <01169235@pw.edu.pl>
2023-09-11 20:20:04 +0200

    minor fix

--- app/components/LoginStatusWrapper.tsx
+++ app/components/LoginStatusWrapper.tsx
@@ -19,8 +19,8 @@ export const LoginStatusWrapper: React.FC<Props> = ({ children }) => {
   }
 
   if (status === "unauthenticated") {
-    return <div className="flex w-full h-screen justify-center text-center">
-      <div className="flex flex-col justify-center items-center gap-5">
+    return <div className="flex w-full h-screen justify-center text-center bg-slate-700 text-white">
+      <div className="flex flex-col justify-center items-center gap-5 ">
         <h1>You need to log in to view this page.</h1>
         <LoginLogout />
       </div>
