766ae0f37ec8b66c3894a6b2c47edd358c234850
ssikors2 <01169235@pw.edu.pl>
2023-09-13 15:46:28 +0200

    Added workout template

--- app/workout-templates/page.tsx
+++ app/workout-templates/page.tsx
@@ -5,6 +5,27 @@ import { useRouter } from "next/navigation";
 import { LoginStatusWrapper } from "../components/LoginStatusWrapper";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { WorkoutTemplate } from "../components/workout-templates/WorkoutTemplate";
+
+import { ExcerciseSets } from "../components/workout-templates/WorkoutTemplate";
+
+const excercises = [
+  {
+    excerciseName: "Pushups",
+    numberOfSets: 4,
+    id: 'id1',
+  },
+  {
+    excerciseName: "Pullups",
+    numberOfSets: 4,
+    id: 'id2',
+  },
+  {
+    excerciseName: "Bicep Curls",
+    numberOfSets: 4,
+    id: 'id3',
+  },
+];
 
 export default function MyProfile() {
   const router = useRouter();
@@ -14,9 +35,18 @@ export default function MyProfile() {
     <LoginStatusWrapper>
       <div className="w-full mt-24 flex flex-col text-white bg-slate-700  min-h-screen h-full items-center justify-start">
         <Header currentPage="My Profile" />
-          <h1 className="text-2xl mt-12">Your Templates:</h1>
-          <div className="flex flex-row">
-          </div>
+        <h1 className="text-2xl mt-12">Your Templates:</h1>
+        <div className="flex flex-col">
+          <WorkoutTemplate
+            title="My workout template"
+            excerciseList={excercises}
+          ></WorkoutTemplate>
+          <WorkoutTemplate
+            title="My workout template 2"
+            excerciseList={excercises}
+          ></WorkoutTemplate>
+          {/* <img src="https://cdn.pixabay.com/photo/2022/02/08/02/52/gym-7000637_1280.png" alt="bruh" /> */}
+        </div>
         <Footer />
       </div>
     </LoginStatusWrapper>
