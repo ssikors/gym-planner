28cc2ed86c6a28d16eed6c756805d5f6cca29c56
ssikors2 <01169235@pw.edu.pl>
2023-09-14 19:01:00 +0200

    Minor fix

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient()
 
-export async function POST(request: any) {
+export async function POST(request: Request) {
   const body = await request.json()
   const { name, email, password} = body.data
 
