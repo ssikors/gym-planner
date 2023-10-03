2c3d06082bb7a55e63366eee5ed3a29553df0579
ssikors2 <01169235@pw.edu.pl>
2023-10-03 15:45:54 +0200

    Adding ExcerciseSelection

---
+++ app/components/excercises/ExcerciseSelection.tsx
@@ -0,0 +1,12 @@
+import { Excercise } from "@prisma/client"
+
+type Props = {
+  excercises: Excercise[]
+  appendExcercise: Function
+}
+
+export const ExcerciseSelection: React.FC<Props> = () => {
+  return <div>
+
+  </div>
+}
